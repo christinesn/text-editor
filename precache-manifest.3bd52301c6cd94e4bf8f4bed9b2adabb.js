9e2c203de12dcfe92d17aad73ebdb79012c3c93c
christinesn <51234471+christinesn@users.noreply.github.com>
2023-09-29 15:06:35 -0400

    Deploy to github pages

---
+++ precache-manifest.3bd52301c6cd94e4bf8f4bed9b2adabb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb09510b28067b583f37153c075bfc6",
+    "url": "/text-editor/index.html"
+  },
+  {
+    "revision": "f2b7c98b3b899b02167b",
+    "url": "/text-editor/static/css/2.1af384ba.chunk.css"
+  },
+  {
+    "revision": "f2b7c98b3b899b02167b",
+    "url": "/text-editor/static/js/2.befdda57.chunk.js"
+  },
+  {
+    "revision": "d6aea9593d3fdd2a593b",
+    "url": "/text-editor/static/js/main.e56887e4.chunk.js"
+  },
+  {
+    "revision": "342736392ed1c583715a",
+    "url": "/text-editor/static/js/runtime-main.f14d9bc3.js"
+  }
+]);
